ede4db3dd8d8c6dc5ada43d62a72762fdd127ab9
ljgshbkcgchi2026-hash <maksimevdokimov2026@gmail.com>
2026-06-28 13:40:45 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB1MDMsnaCZvCw2L5iGF_q8VXRmk7weKJo",
+  authDomain: "pingapi-a3d1f.firebaseapp.com",
+  projectId: "pingapi-a3d1f",
+  storageBucket: "pingapi-a3d1f.firebasestorage.app",
+  messagingSenderId: "193814492426",
+  appId: "1:193814492426:web:9871aaa7ca23582b2b36f3"
+};
